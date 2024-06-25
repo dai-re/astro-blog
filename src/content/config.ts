14eab8b694e231157cb7d0014c688e0f30dbf3dd
dai-re <dairewahandi@gmail.com>
2024-06-25 22:06:51 +0700

    test: :fire: init
    goobrrrr
    
    nope

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content';
+
+const post = defineCollection({
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.string(),
+      admin: z.string(),
+      desc: z.string(),
+      image: image(),
+      tags: z.array(z.string()),
+    }),
+});
+
+export const collections = { post };
---
+++ src/pages/rss.xml.js
@@ -0,0 +1,22 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET() {
+  const articles = await getCollection('post');
+  const all = articles.sort((a, b) => new Date(b.data.date).getTime() - new Date(a.data.date).getTime());
+
+  return rss({
+    title: 'Daire Blog',
+    description: "I'ts my blog website",
+    site: 'http://daire.eu.org/',
+    items: all.map((article) => ({
+      title: article.data.title,
+      description: article.data.desc,
+      pubDate: article.data.date,
+      link: `/post/${article.slug}/`,
+      customData: `
+      <author>${article.data.admin}</author>
+    `,
+    })),
+  });
+}
